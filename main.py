188810c2725073edff397cf10cbfb741a0157fe3
Silva1616 <goncalosilva.7002@gmail.com>
2026-05-01 23:31:05 +0100

    Página com streamlit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Text Normalizer",
+    page_icon="📄",
+    layout="wide"
+)
+
+st.title("Text Normalizer - Pipeline de Pré processamento")
+st.markdown("---")
